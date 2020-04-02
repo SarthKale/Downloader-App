cd4e77e59989a4932212b57096b3410c3f9ea0ff
unknown <sarthak.kale12@gmail.com>
2020-04-02 13:07:20 +0530

    The new Ammendments to the project.

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Downloader App
-
+This is a Desktop Application built in Python Language which we can use to download various types of files whose links are available online. Only the thing to note is that while giving file-name, we need to specify it's extention with the file-name(Ex: abc.docx).
 
 ## What
-
+It is an Application that can download 3 variety of files whose links are available online.
+1. Any form of File like Document or Software Installation Packages or Videos from various sites.
+2. Any video which is present in the youtube database.
+3. An entire playlist of videos directly from the youtube.
+The Application contains 4 pages and a nevigation button-set at the left margin and can be utilized in 4 different themes.
 
 ## How to setup and run
+Extract the Zip file and then run the file with .exe extention present in the dist named sub-folder of the extractable folder.
--- index.py
+++ index.py
@@ -1,3 +1,7 @@
+"""
+Downloader-App:- This is a Python Language program to built a Desktop Application in  which we can use to download various types of files like documents, videos, installer packages, etc, whose links are available online. 
+Only the thing to note is that while giving file-name, we need to specify it's extention with the file-name(Ex: abc.docx).
+"""
 import sys
 import os
 from PyQt5.QtGui import *
@@ -10,9 +14,14 @@ import humanize
 
 ui, _ = loadUiType('main.ui')
 
-
+"""
+Main:- This is the class that integrates all the functionalities of the application and manages them according to their respective calls.
+"""
 class Main(QMainWindow, ui):
 
+    """
+    __init__:- This is the constructor method of the class responsible for initiating the application developed.
+    """
     def __init__(self, parent=None):
         super(Main, self).__init__(parent)
         QMainWindow.__init__(self)
@@ -20,6 +29,9 @@ class Main(QMainWindow, ui):
         self.initUi()
         self.handleButton()
 
+    """
+    initUi:- This method enables the initial UI of the application.
+    """
     def initUi(self):
         '''Contains all UI chnges in loading'''
         print("UI Initiated")
@@ -31,6 +43,9 @@ class Main(QMainWindow, ui):
         self.moveGBox3()
         self.moveGBox4()
 
+    """
+    handleButton:- This method operates and manages the buttons and corresponding calls for the respective methods.
+    """
     def handleButton(self):
         '''Maintains the Functionalities of the Butttons'''
         print("Button Clicked")
@@ -65,6 +80,15 @@ class Main(QMainWindow, ui):
         self.playlistDownload.clicked.connect(self.pageCall3)
         self.setChanges.clicked.connect(self.pageCall4)
 
+    """
+    handleProgress:- This method manages and controls the progress display bar for the file being downloaded.
+    Variables:-
+        totalsize(int) - holds total number of bits present in the file.
+        read_data(int) - holds the total data downloaded.
+        dpercent(int) - holds the ratio of read_data to totalsize.
+        blocksize(int) - holds the size of each block of bits.
+        blocknum(int) - holds the index for currently downloading block.
+    """
     def handleProgress(self, blocknum, blocksize, totalsize):
         """Calculates and Manages Progress"""
         print("Progress Started")
@@ -74,12 +98,23 @@ class Main(QMainWindow, ui):
             self.progressBar_t1.setValue(dpercent)
             QApplication.processEvents()
 
+    """
+    handleBrowse:- This method is used to refer the download location of the file to the application.
+    Variables:-
+        location(string) - holds the destination address where the file is needed to be downloaded.
+    """
     def handleBrowse(self):
         '''Manages Browsing within the OS'''
         print("Browsing Location")
         location = QFileDialog.getSaveFileName(self, caption="Save As", directory=".", filter="All Files(*.*)")
         self.location_t1.setText(str(location[0]))
 
+    """
+    download:- This method is used to perform the generic downloading which occurs according to the bit-data of the file.
+    Variable:-
+        url(string) - holds the actual url of the file.
+        location(string) - holds the destination address where the file is needed to be downloaded.
+    """
     def download(self):
         '''Controls Downloading of files'''
         print("Download Started")
@@ -98,6 +133,15 @@ class Main(QMainWindow, ui):
         self.location_t1.setText('')
         self.progressBar_t1.setValue(0)
 
+    """ 
+    get_VideoData:- This method is used to retrive data about the video from youtube using a pafy object.
+    Variables:- 
+        vurl(string) - holds the actual url of the video.
+        video(pafy object) - holds the pafy type instance of the video.
+        qualities(array of strings) - holds the value of all possible qualities for which the video is available.
+        size(string) - holds the size of the file for corresponding quality.
+        data(string) - displays entire data regarding a quality for the video.
+    """
     def get_VideoData(self):
         '''Utilizes Video related Data'''
         print("Video Data Retrival")
@@ -113,11 +157,26 @@ class Main(QMainWindow, ui):
             data = "{} {} {} {}".format(stream.mediatype, stream.extension, stream.quality, size)
             self.vquality.addItem(data)
 
+    """ 
+    videoBrowse:- This method is used to refer the download location of the video to the application.
+    Variables:-
+        location(string) - holds the destination address where the video is needed to be downloaded.
+    """
     def videoBrowse(self):
         print("Browse Video")
         location = QFileDialog.getSaveFileName(self, caption="Save As", directory=".", filter="All Files(*.*)")
         self.vlocation_t2.setText(str(location[0]))
 
+    """ 
+    downloadVideo:- This method calls for the video from the online database and controls and manages it's downloading.
+    Variables:- 
+        vurl(string) - holds the actual url of the playlist.
+        vlocation(string) - holds the destination address where the video is needed to be downloaded.
+        quality(string) - holds the quality at which the video is to be downloaded.
+        current_video(pafy object) - holds the pafy type instance of a single video.
+        vstream(quality-refering object) - holds the available qualities present for the video.
+        download(bit-object) - holds and downloads data bitwise.
+    """
     def downloadVideo(self):
         print("Youtube Video Download")
         vurl = self.vurl_t2.text()
@@ -134,6 +193,17 @@ class Main(QMainWindow, ui):
         self.vlocation_t2.setText('')
         self.vprogressBar_t2.setValue(0)
 
+    """ 
+    videoProgress:- This method manages and controls the progress display bar for the video being downloaded.
+    Variables:-
+        total(int) - holds total number of frames present in the video.
+        recieved(int) - holds the total data downloaded.
+        ratio(int) - holds the ratio of total to recieved.
+        time(time object) - holds the estimated time suggested by the youtube.
+        rate(int) - holds the ratio of time(in seconds) to total undownloaded data.
+        remtime(int) - holds the actual remaining time for video that is being downloaded.
+        st(string) - used to display the remtime on screen while the download is in progress.
+    """
     def videoProgress(self, total, recieved, ratio, rate, time):
         print("Video Progress Start")
         read_data = recieved
@@ -145,11 +215,29 @@ class Main(QMainWindow, ui):
             self.vrem.setText(st)
             QApplication.processEvents()
 
+    """ 
+    playlistBrowse:- This method is used to refer the download location of the playlist to the application.
+    Variables:-
+        location(string) - holds the destination address where the playlist is needed to be downloaded.
+    """
     def playlistBrowse(self):
         print("Browse Playlist")
         location = QFileDialog.getExistingDirectory(self, "Select Download Directory")
         self.plocation_t2.setText(str(location[0]))
 
+    """
+    downloadPlaylist:- This method calls for the playlist from the online database and controls and manages it's downloading.
+    Variables:- 
+        purl(string) - holds the actual url of the playlist.
+        plocation(string) - holds the destination address where the playlist is needed to be downloaded.
+        playlist(pafy object) - holds the pafy type instance of the playlist used to refer data from youtube.
+        plvideos(sub-pafy object) - holds the pafy type instance of a single video from the playlist, used to refer data from youtube.
+        vcurrent(int) - holds the currently downloading video's position in playlist.
+        quality(string) - holds the quality at which the video is to be downloaded.
+        current_video(pafy object) - holds the pafy type instance of a single video.
+        vstream(quality-refering object) - holds the quality to be downloaded for the current downloading video.
+        download(bit-object) - holds and downloads data bitwise.
+    """
     def downloadPlaylist(self):
         print("Youtube Playlist Download")
         purl = self.purl_t2.text()
@@ -157,7 +245,7 @@ class Main(QMainWindow, ui):
         if purl == '' or plocation == '':
             QMessageBox.warning(self, "Invalid Entries", "Invalid URL or Browse Location")
         else:
-            playlist = pafy.get_playlist(purl, ydl_opts={"nocheckcertificate": True})
+            playlist = pafy.get_playlist(purl)
             plvideos = playlist["items"]
             self.tvlcd.display(len(plvideos))
             print(len(plvideos))
@@ -173,10 +261,21 @@ class Main(QMainWindow, ui):
                 self.cvlcd.display(vcurrent)
                 current_video = video["pafy"]
                 vstream = current_video.streams
-                download = vstream[quality].download(callback=self.playlistProgress)
+                download = vstream[quality].download(callback=self.playlistProgress, ydl_opts={"nocheckcertificate": True})
                 QApplication.processEvents()
                 vcurrent += 1
 
+    """ 
+    playlistProgress:- This method manages and controls the progress display bar for every video present in the playlist being downloaded.
+    Variables:-
+        total(int) - holds total number of videos present in the playlist.
+        recieved(int) - holds the total data downloaded.
+        ratio(int) - holds the ratio of total to recieved.
+        time(time object) - holds the estimated time suggested by the youtube.
+        rate(int) - holds the ratio of time(in seconds) to total undownloaded data.
+        remtime(int) - holds the actual remaining time for each video that is being downloaded.
+        st(string) - used to display the remtime on screen while the download is in progress.
+    """
     def playlistProgress(self, total, recieved, ratio, rate, time):
         print("Playlist Progress Start")
         read_data = recieved
@@ -188,6 +287,9 @@ class Main(QMainWindow, ui):
             self.prem.setText(st)
             QApplication.processEvents()
 
+    """ 
+    pageShifts:- The nevigation buttons present in the left margin of the application can be utilized to directly nevigate between pages. The following 4 methods controls the narration descriped.
+    """
     def pageHome(self):
         print("Home Page")
         self.tabWidget.setCurrentIndex(0)
@@ -204,6 +306,11 @@ class Main(QMainWindow, ui):
         print("Settings Page")
         self.tabWidget.setCurrentIndex(3)
 
+    """ 
+    theme:- The Application can be operated in 4 themes namely Dark Orange, Default, Dark and Dark Blue. The following 4 methods are used to alter between various themes available.
+    Variable:-
+        style(file object) - For all the following 4 methods this variables holds the instance for altering between the themes.
+    """
     def themeDarkOrange(self):
         print("Dark Orange Theme")
         style = open("themes/darkorange.css", "r")
@@ -228,6 +335,12 @@ class Main(QMainWindow, ui):
         style = style.read()
         self.setStyleSheet(style)
 
+    """ 
+    moveGBOX :- The following 4 methods gives animation feature just once when the application is launched.
+    Variables:- 
+        animation1, animation2, animation3, animation4(objects) -
+            They all hold instances for animation call for the followinf 4 methods, respectively.
+    """
     def moveGBox1(self):
         # GBox1 Animation
         print("GBox1")
@@ -268,6 +381,9 @@ class Main(QMainWindow, ui):
         self.animation4 = animation4
         animation4.start()
 
+    """ 
+    pageCalls :- The application consist of 4 major pages(Home, Generic Download, Youtube Download, Settings) and each of the following 4 methods manages the switching between these pages.
+    """
     def pageCall1(self):
         print("Call Solo Download")
         self.tabWidget.setCurrentIndex(1)
@@ -286,7 +402,12 @@ class Main(QMainWindow, ui):
         print("Call Settings Page")
         self.tabWidget.setCurrentIndex(3)
 
-
+""" 
+major :- This is the Main method that begins and controls the execution of the complete program.
+Variables :-
+    app(object) - holds the Instance of the application as whole.
+    window(object) - holds the display mechanism of the entire application.
+"""
 def major():
     app = QApplication(sys.argv)
     window = Main()
